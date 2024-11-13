8b07017133349a60d491018e3abf8ee0b0233adb
Enderson Quiroz <endequirozca@cesde.net>
2024-11-12 23:04:15 -0500

    se realiza toda la logica de javaScrit segun el DOM en la pagina de contactos

---
+++ controllers/controllerContact.js
@@ -0,0 +1,13 @@
+export function enviar(){
+    let nombre = document.getElementById('nombre').value
+    let email = document.getElementById('correoElectronico').value
+    let mensaje = document.getElementById('mensaje').value
+    console.log(nombre);
+    console.log(correoElectronico);
+    console.log(mensaje);
+    
+    
+    
+
+    console.log('Enviar informacion');
+}
