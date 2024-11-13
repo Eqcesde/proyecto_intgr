8d350184f187e60d604d47f5a4f4c7f1797b69b4
Enderson Quiroz <endequirozca@cesde.net>
2024-11-13 08:14:45 -0500

    Se implementa modulos en vista contactos

--- controllers/controllerContact.js
+++ controllers/controllerContact.js
@@ -1,13 +1,5 @@
 export function enviar(){
     let nombre = document.getElementById('nombre').value
-    let email = document.getElementById('correoElectronico').value
+    let correoElectronico = document.getElementById('correoElectronico').value
     let mensaje = document.getElementById('mensaje').value
-    console.log(nombre);
-    console.log(correoElectronico);
-    console.log(mensaje);
-    
-    
-    
-
-    console.log('Enviar informacion');
 }
