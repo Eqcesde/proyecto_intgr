29e77a398a47818eddd5a02f26d680fd391afa2e
BellaCoder99 <iszp99@gmail.com>
2024-11-14 18:23:20 -0500

    Se agrega la clase Servicios y sus respectivas clases  y objetos- Java

---
+++ backend/models/Servicios.java
@@ -0,0 +1,15 @@
+public class Servicios {
+    private int servicio_id;
+    private String nombre_Servicio;
+    
+    public Servicios(String servicio_id, String nombre_Servicio){
+        this.servicio_id = servicio_id;
+        this.nombre_Servicio = servicio;
+
+    }
+
+    public static void main (String[] args){
+        Servicios servicios = new servicios(01,"carpintería");
+        System.out.println("Nombre del servicio:" + servicio.getservicio());
+    }
+}
