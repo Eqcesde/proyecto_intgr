5283311efc19ae7559fdbd0c8022299265b1c6d1
Enderson Quiroz <endequirozca@cesde.net>
2024-11-15 13:30:39 -0500

    Se actualizo toda la parte de backend y se corrigieron algunos errores de codigo

--- backend/models/Proveedores.java
+++ backend/models/Proveedores.java
@@ -8,7 +8,6 @@ public class Proveedores {
     public Proveedores (String proveedor_id, String nombre_proveedor,  int telefono, String direccion, String correo){
         this.proveedor_id = proveedor_id;
         this.nombre_proveedor = nombre_proveedor;
-        this.contacto = contacto;
         this.telefono = telefono;
         this.direccion = direccion;
         this.correo = correo;
@@ -21,13 +20,14 @@ public class Proveedores {
         return nombre_proveedor;
     }
     public int getTelefono(){
-        this.telefono = telefono;
+        return telefono;
+
     }
     public String getDireccion(){
-        this.direccion=direccion;
+        return direccion;
     }
     public String getCorreo(){
-        this.correo=correo;
+        return correo;
     }
 
     public void setNombre_proveedor(String nombre_proveedor){
@@ -43,7 +43,11 @@ public class Proveedores {
 
         public static void Main(String[] args){
             Proveedores proveedor = new Proveedores("001", "Proveedor 1", 1234567890, "Calle Falsa 123", "proveedor1@correo.com");
+
+            System.out.println("Identidad del Proveedor: " + proveedor.getProveedor_id());
             System.out.println("Nombre del proveedor: " + proveedor.getNombre_proveedor());
+            System.out.println("Telefono Proveedor: " + proveedor.getTelefono());
+            System.out.println("Direccion del Proveedor: " + proveedor.getDireccion());
+            System.out.println("Correo del Proveedor: " + proveedor.getCorreo());
         }
     }
-}
