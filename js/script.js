e2b5b912216f2e4154dd0ca2e07138fe0d73d6f6
Enderson Quiroz <endequirozca@cesde.net>
2024-11-23 19:00:23 -0500

    se modifica todo el js, se agrega tablas, y se enlasa con el controller, model y views con la TablaRegistro.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 import { listarRegistros } from "../controllers/controllerRegistrate.js";
 
 document.getElementById("btnRegistro").addEventListener('click', listarRegistros);
+
+import { crearRegistroContact } from "../controllers/controllerContact.js";
+
+document.getElementById("btnEnviar").addEventListener('click', crearRegistroContact);
