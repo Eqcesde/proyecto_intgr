8346ff7577697c5362a3e7867a7d9f4fd163c8e2
Edwinmamu <163479271+Edwinmamu@users.noreply.github.com>
2024-11-25 22:11:17 -0500

    se organiza el modelContact.js

--- models/modelContact.js
+++ models/modelContact.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export let contactos = [
     {
       nombre: "INSY HOGAR",
@@ -6,14 +6,5 @@ export let contactos = [
       mensaje: "Hola Somos El Grupo 3 y Este Es Nuestro Icomers de Servicios ",
     },
   ];
-=======
 
-export let contactos = [
-  {
-    nombre: "INSY HOGAR",
-    email: "insyHogar@gmail.Com",
-    mensaje: "Hola Somos El Grupo 3 y Este Es Nuestro Icomers de Servicios ",
-  },
-];
 
->>>>>>> 4f3bda2f645554831751bd3fb593b59b2939fa64
