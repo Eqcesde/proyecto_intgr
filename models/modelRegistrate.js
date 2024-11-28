07a14455e179a2f8a827639d411c2ea223c26f04
Enderson Quiroz <endequirozca@cesde.net>
2024-11-27 19:51:01 -0500

    se modifica el index, tabla registro y registrate, se le hacen mejoras ala parte de javascript

--- models/modelRegistrate.js
+++ models/modelRegistrate.js
@@ -1,6 +1,32 @@
 export let registros = [
   {
-    email: "correo@correo.com",
-    password: "12345",
+    nombre: "Enderson Quiroz",
+    telefono: "3101234567",
+    direccion: "Calle 10 #45-32, Medellín",
+    email: "enderson.quiroz@gmail.com",
+  },
+  {
+    nombre: "Edwin Martinez",
+    telefono: "3209876543",
+    direccion: "Carrera 15 #25-50, Medellín",
+    email: "edwin.montoya@gmail.com",
+  },
+  {
+    nombre: "Isabela Pedraza",
+    telefono: "3115678901",
+    direccion: "Avenida 80 #30-45, Medellín",
+    email: "isabela.restrepo@gmail.com",
+  },
+  {
+    nombre: "Steven Calle",
+    telefono: "3123456789",
+    direccion: "Calle 45 #60-10, Medellín",
+    email: "steven.gomez@gmail.com",
+  },
+  {
+    nombre: "Camila Torres",
+    telefono: "3006543210",
+    direccion: "Carrera 70 #80-20, Medellín",
+    email: "camila.torres@gmail.com",
   },
 ];
